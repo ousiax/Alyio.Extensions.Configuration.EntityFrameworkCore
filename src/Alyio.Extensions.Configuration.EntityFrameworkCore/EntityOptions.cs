0baf52c65ad138ba7e4d30656c65100346e7632d
XU <qqbuby@gmail.com>
2022-06-16 11:41:46 +0800

    Support Entity Framework Core 3.x

--- src/Alyio.Extensions.Configuration.EntityFrameworkCore/EntityOptions.cs
+++ src/Alyio.Extensions.Configuration.EntityFrameworkCore/EntityOptions.cs
@@ -3,7 +3,27 @@
 /// <summary>
 /// Options used to configure the entity type.
 /// </summary>
-/// <param name="TableName">Specifies the database table that the configuration entity is mapped to.</param>
-/// <param name="KeyColumnName">Specifies the name of the column the configuration key is mapped to.</param>
-/// <param name="ValueColumeName">Specifies the name of the column the configuration value is mapped to.</param>
-internal record EntityOptions(string TableName, string KeyColumnName, string ValueColumeName);
+internal record EntityOptions
+{
+    public EntityOptions(string table, string key, string value)
+    {
+        TableName = table;
+        KeyColumnName = key;
+        ValueColumeName = value;
+    }
+
+    /// <summary>
+    /// Specifies the database table that the configuration entity is mapped to.
+    /// </summary>
+    public string TableName { get; }
+
+    /// <summary>
+    /// Specifies the name of the column the configuration key is mapped to.
+    /// </summary>
+    public string KeyColumnName { get; }
+
+    /// <summary>
+    /// Specifies the name of the column the configuration value is mapped to.
+    /// </summary>
+    public string ValueColumeName { get; }
+}
